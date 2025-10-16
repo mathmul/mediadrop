bb114c77a790794110e0059b5fadd279b9d79a7c
Matej Mulej <mmulej@gmail.com>
2025-10-16 22:06:25 +0200

    Add CORS middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // run CORS before everything else
+        $middleware->prepend(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
---
+++ config/cors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods'   => ['*'],
+    'allowed_origins'   => [
+        'http://localhost:5173',
+        'https://mediadrop.test',
+    ],
+    'allowed_origins_patterns' => [
+        '/^https?:\/\/([a-z0-9-]+\.)?mediadrop\.test$/',
+    ],
+    'allowed_headers'   => ['*'],
+    'exposed_headers'   => [],
+    'max_age'           => 3600,
+    'supports_credentials' => false,
+];
